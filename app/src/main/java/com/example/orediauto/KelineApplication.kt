06f01cd76e42bc7a88b412ea01c29a89ee1b1c0b
eugeneoredi <eugeneoredi5@gmail.com>
2024-05-13 23:14:19 +0300

    first commit

---
+++ app/src/main/java/com/example/orediauto/KelineApplication.kt
@@ -0,0 +1,9 @@
+package com.example.orediauto
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class KelineApplication: Application() {
+
+}
---
+++ app/src/main/java/com/example/orediauto/fragments/shopping/SearchFragment.kt
@@ -0,0 +1,7 @@
+package com.example.orediauto.fragments.shopping
+
+import androidx.fragment.app.Fragment
+import com.example.orediauto.R
+
+class SearchFragment: Fragment(R.layout.fragment_search) {
+}
